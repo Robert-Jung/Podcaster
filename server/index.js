e0d9a46ad50b2a49d04cb35233e87f44ef2304bc
Robert-Jung <rjinjung@gmail.com>
2017-05-09 14:36:58 -0700

    PUT podcasts in level db and GET podcasts #1

--- server/index.js
+++ server/index.js
@@ -7,6 +7,17 @@ const podcasts = require('./seed.js')
 
 app.use(respondStatic)
 
+db.put('podcasts', JSON.stringify(podcasts), err => {
+  if (err) return console.log('Opps!', err)
+})
+
+db.get('podcasts', (err, value) => {
+  const podcasts = JSON.parse(value)
+  podcasts.map((podcast) => {
+    console.log(podcast.title)
+  })
+})
+
 app.listen(3000, () => {
   console.log('Listening on port 3000')
 })
