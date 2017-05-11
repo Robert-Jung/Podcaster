bb5b750c34d53ab98fe5a821b59c41e439aac053
Robert-Jung <rjinjung@gmail.com>
2017-05-11 14:20:50 -0700

    Adding search input button #6

--- client/searchInput.js
+++ client/searchInput.js
@@ -2,11 +2,22 @@ const React = require('react')
 const TextField = require('material-ui/TextField').default
 const MuiThemeProvider = require('material-ui/styles/MuiThemeProvider').default
 const colors = require('material-ui/styles/colors')
+const RaisedButton = require('material-ui/RaisedButton').default
+const FontIcon = require('material-ui/FontIcon').default
+const ActionSearch = require('material-ui/svg-icons/action/search').default
+
+const style = {
+  margin: '20px',
+}
 
 const SearchInput = () => {
   return (
     <form>
       <TextField floatingLabelText='Search for Podcast'/>
+      <RaisedButton
+      icon={<ActionSearch />}
+      style={style}
+      />
     </form>
   )
 }
